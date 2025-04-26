2ff771c59492b921e911713014cbeb1cde67fe64
anachapon <ana.chapon@gmail.com>
2025-04-26 22:43:57 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+class Parquimetro {
+    constructor(valorPago){
+        this.valorPago = valorPago;
+        this.tempo = 0;
+        this.valorPagar = 0;
+        this.valorMin= 1.0;
+
+    }
+    
+}
+
+parquimetro.calcular() {
+const parquimetro = new Parquimetro (document.getElementById("valor")).value;
+
+saldo(valor){
+if (this.valorPago < 1)
+    document.getElementById ("insuficiente").textContent = "O valor mínimo é R$ 1,00"
+}
+
+
+tempo(valor){
+    
+    
+}
+
+troco(valor){
+    
+    
+}
+}
+
