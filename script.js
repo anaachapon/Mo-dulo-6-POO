0d82e98c667b11785bdb3917b8aa4f590ef7a11e
anachapon <ana.chapon@gmail.com>
2025-05-01 18:17:53 +0100

    new

--- script.js
+++ script.js
@@ -1,31 +1,86 @@
 class Parquimetro {
-    constructor(valorPago){
+    constructor(valorPago) {
         this.valorPago = valorPago;
         this.tempo = 0;
-        this.valorPagar = 0;
-        this.valorMin= 1.0;
+        this.valorMin = 1.0;
+        this.valorMax = 3.0;
+        this.troco = 0;
+        this.error = false;
 
     }
-    
+    calcular() {
+        if (this.valorPago < this.valorMin || this.valorPago > this.valorMax || Number.isNaN(this.valorPago)) {
+            this.error = true;
+
+        } else if (this.valorPago >= this.valorMin && this.valorPago < 1.75) {
+            this.tempo = 30;
+            this.troco = this.calcularTroco(this.valorMin)
+              
+
+        } else if (this.valorPago >= 1.75 && this.valorPago < this.valorMax) {
+            this.tempo = 60;
+            this.troco = this.calcularTroco(1.75)
+           
+        }
+
+        else if (this.valorPago = this.valorMax) {
+            this.tempo = 120;
+        
+        }
+
+        return this;
+
+    }
+    calcularTroco(valor) {
+        return (this.valorPago - valor).toFixed(2)
+
+    }
+
 }
 
-parquimetro.calcular() {
-const parquimetro = new Parquimetro (document.getElementById("valor")).value;
+document.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-saldo(valor){
-if (this.valorPago < 1)
-    document.getElementById ("insuficiente").textContent = "O valor mínimo é R$ 1,00"
-}
+    let valorPago = parseFloat(document.getElementById("valorPago").value);
 
+    const parquimetro = new Parquimetro(valorPago)
 
-tempo(valor){
+    let troco = parquimetro.calcular();
+
+    if (parquimetro.error) {
+        document.getElementById("insuficiente").textContent = "Insira um valor entre 1,00 e 3,00";
+        return;
+    }
+
+    else if(this.valorPago >= this.valorMin && this.valorPago < 1.75){
+        document.getElementById("tempoD").textContent = "Tempo: 30 minutos";
+
+    }
+
+    else if(this.valorPago >= 1.75 && this.valorPago < this.valorMax){
+        document.getElementById("tempoD").textContent = "Tempo: 60 minutos";
+
+    }
+
+    else if(this.valorPago = this.valorMax){
+        document.getElementById("tempoD").textContent = "Tempo: 120 minutos";
+       }
+
     
-    
-}
 
-troco(valor){
-    
-    
-}
-}
 
+
+    
+
+console.log(parquimetro.tempo)
+console.log(parquimetro.troco)
+
+
+
+});
+
+//* } else if (this.valorPago >= 1.75 && this.valorPago < 3.0){
+//*  this.tempo = 60;
+//*  this.troco = this.valorPago - 1.75;
+
+//* } proximo e colocar as infos no html
